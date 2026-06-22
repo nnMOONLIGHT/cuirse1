75daf47b724eee8a049428c6e1e74ec3e828f9da
nnMOONLIGHT <150700304+nnMOONLIGHT@users.noreply.github.com>
2026-06-22 10:26:19 +0300

    Update set-webhook.bat

--- projects/ChatBot/scripts/set-webhook.bat
+++ projects/ChatBot/scripts/set-webhook.bat
@@ -3,11 +3,11 @@ chcp 65001 >nul
 echo === Настройка webhook для CuirseBot ===
 echo.
 
-set /p BOT_TOKEN="Введите токен бота Telegram: "
-set /p TUNNEL_URL="Введите URL туннеля (например https://xxx.loca.lt): "
+set /p BOT_TOKEN="8781642227:AAELlMriNHN5xno_GexlnSLWiuypRVC36Pc"
+set /p TUNNEL_URL=""
 
-curl -s "https://api.telegram.org/bot%BOT_TOKEN%/setWebhook?url=%TUNNEL_URL%/api/update"
+curl -s "https://api.telegram.org/8781642227:AAELlMriNHN5xno_GexlnSLWiuypRVC36Pc/setWebhook?url=/api/update"
 echo.
 echo.
-curl -s "https://api.telegram.org/bot%BOT_TOKEN%/getWebhookInfo"
+curl -s "https://api.telegram.org/8781642227:AAELlMriNHN5xno_GexlnSLWiuypRVC36Pc/getWebhookInfo"
 echo.
